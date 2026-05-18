b18fe23d832eb9f982442c779fc0d29bf7cb72be
aavishkar65-web <aavishkar65@gmail.com>
2026-05-18 16:15:22 +0530

    Header&Footer

---
+++ Book_An_Appointment1/Pages/Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Book_An_Appointment1.Pages
+{
+    public class FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Book_An_Appointment1/Program.cs
+++ Book_An_Appointment1/Program.cs
@@ -79,3 +79,4 @@ app.MapGet("/", context =>
 });
 
 app.Run();
+app.UseStaticFiles();
